e09999d7e8853f3c097ee9d073364592c7f9d208
SaveFeelix <felixhake86@gmail.com>
2021-07-19 16:22:30 +0200

    Add static Methodes to ISection.java

--- src/main/java/de/savefeelix/section/interfaces/ISection.java
+++ src/main/java/de/savefeelix/section/interfaces/ISection.java
@@ -1,6 +1,12 @@
 package de.savefeelix.section.interfaces;
 
+import de.savefeelix.utils.Registry;
 import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public interface ISection {
 
@@ -10,5 +16,35 @@ public interface ISection {
      */
     @NotNull
     String getName();
+
+    /**
+     * Get all REGISTERED Sections
+     * @return List<ISection>
+     */
+    static List<ISection> all() {
+        List<ISection> sections = new ArrayList<>();
+        Registry.registeredSections.forEach(section -> {
+            try {
+                sections.add(section.getConstructor().newInstance());
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        });
+        return sections;
+    }
+
+    /**
+     * Get Section by Name
+     * @param name String
+     * @return ISection
+     */
+    static ISection byName(String name) {
+        AtomicReference<ISection> tmpSection = new AtomicReference<>(null);
+        all().forEach(section -> {
+            if (section.getName().equalsIgnoreCase(name))
+                tmpSection.set(section);
+        });
+        return tmpSection.get();
+    }
 
 }
